7d2bd741f5f9b87dfcf7c4b6616ec7bffc636194
Gentil-N <ngentil77@gmail.com>
2022-04-04 17:15:49 +0200

    Doc d2 updated

--- src/math/integration/d2.rs
+++ src/math/integration/d2.rs
@@ -3,6 +3,19 @@
 
 /// # (d2) Rectangle rule
 /// Integrate two-dimensional function through the rectangle rule.
+/// * `function` - Closure or function pointer matching `f(x, y) = z`
+/// * `x_lower_bound` - lower bound of the first integral (Fixed value)
+/// * `x_upper_bound` - upper bound of the first integral (Fixed value)
+/// * `y_lower_bound` - lower bound of the second integral (Closure or function pointer)
+/// * `y_upper_bound` - upper bound of the second integral (Closure or function pointer)
+/// * `div_x` - Number of chunk evaluated for the first variable: _big_ = high precision & low performances, _small_ = high performances & low precision
+/// * `div_y` - Same as `div_x` for the second variable
+/// 
+/// ```
+/// # use scilib::math::integration::d2::*;
+/// let res = rect(|x, y| x * y, -4.0, 7.0, |x| x / 2.0, |x| 2.0* x, 10000, 10000);
+/// assert!((res - 1005.46875).abs() < 1.0);
+/// ```
 pub fn rect(
     function: impl Fn(f64, f64) -> f64,
     x_lower_bound: f64,
@@ -49,6 +62,19 @@ fn trapz_sub_y(
 
 /// # (d2) Trapezoidal rule
 /// Integrate two-dimensional function through the trapezoidal rule.
+/// * `function` - Closure or function pointer matching `f(x, y) = z`
+/// * `x_lower_bound` - lower bound of the first integral (Fixed value)
+/// * `x_upper_bound` - upper bound of the first integral (Fixed value)
+/// * `y_lower_bound` - lower bound of the second integral (Closure or function pointer)
+/// * `y_upper_bound` - upper bound of the second integral (Closure or function pointer)
+/// * `div_x` - Number of chunk evaluated for the first variable: _big_ = high precision & low performances, _small_ = high performances & low precision
+/// * `div_y` - Same as `div_x` for the second variable
+/// 
+/// ```
+/// # use scilib::math::integration::d2::*;
+/// let res = trapz(|x, y| x * y, -4.0, 7.0, |x| x / 2.0, |x| 2.0* x, 10000, 100);
+/// assert!((res - 1005.46875).abs() < 1.0e-3);
+/// ```
 pub fn trapz(
     function: impl Fn(f64, f64) -> f64 + Copy,
     x_lower_bound: f64,
@@ -80,18 +106,3 @@ pub fn trapz(
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-#[test]
-fn test_d2() {
-    /*use std::f64::consts::PI;
-    {
-        println!(
-            "value : {}",
-            rect(|x, y| x * y, -4.0, 7.0, |x| x / 2.0, |x| 2.0* x, 10000, 10000)
-        );
-        println!(
-            "value : {}",
-            trapz(|x, y| x * y, -4.0, 7.0, |x| x / 2.0, |x| 2.0* x, 10000, 100)
-        );
-    }*/
-}
