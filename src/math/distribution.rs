1c0308bfbb3eb86bec811f460728614f4542d2c3
At0micBee <hugo.vivien@protonmail.com>
2023-07-16 18:54:50 +0200

    Documenting distributions

--- src/math/distribution.rs
+++ src/math/distribution.rs
@@ -16,45 +16,67 @@ use super::{
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/// # Rayleigh distribution
+/// 
+/// ## Definition
+/// The [Rayleigh distribution](https://en.wikipedia.org/wiki/Rayleigh_distribution) is a continuous
+/// probability distribution for any $x>=0$. It is defined as:
+/// $$
+/// f_\sigma(x) = \frac{x}{\sigma^2} \exp\left( -\frac{x^2}{2\sigma^2} \right)
+/// $$
+/// 
+/// ## Inputs
+/// - `sigma`: the scale parameter of the distribution ($\sigma$)
+/// - `x`: the value at which to evaluate the function ($x$).
+/// 
+/// Returns the density probability at a given point `x` for the `sigma`
+/// scale parameter.
+/// 
+/// ## Example
+/// ```
+/// # use scilib::math::distribution::rayleigh;
+/// let res = rayleigh(0.5, 0.9);
+/// assert!((res - 0.7124353167010128).abs() < 1e-15);
+/// ```
 pub fn rayleigh(sigma: f64, x: f64) -> f64 {
     assert!(x >= 0.0);  // Rayleigh needs to be positive
-    x * (-x.powi(2) / (2.0 * sigma.powi(2))) / sigma.powi(2)
+    x * ( -0.5 * (x / sigma).powi(2) ).exp() / sigma.powi(2)
 }
 
-pub fn rayleigh_vec(sigma: f64, x: &[f64]) -> Vec<f64> {
+/* pub fn rayleigh_vec(sigma: f64, x: &[f64]) -> Vec<f64> {
     assert!(series::min_slice(x) >= 0.0);           // Rayleigh needs to be positive
     let frac_1_sigma2: f64 = 1.0 * sigma.powi(2);   // Pre-computing the factor
 
     x.iter().map(|v| v * frac_1_sigma2 * (-v.powi(2) * frac_1_sigma2 / 2.0)).collect()
-}
+} */
 
 pub fn cauchy(gamma: f64, x0: f64, x: f64) -> f64 {
     FRAC_1_PI * gamma / ((x - x0).powi(2) + gamma.powi(2))
 }
 
-pub fn cauchy_vec(gamma: f64, x0: f64, x: &[f64]) -> Vec<f64> {
+/* pub fn cauchy_vec(gamma: f64, x0: f64, x: &[f64]) -> Vec<f64> {
     let pi_gamma: f64 = FRAC_1_PI * gamma;
     let gamma2: f64 = gamma.powi(2);
     x.iter().map(|v| pi_gamma / ((v - x0).powi(2) + gamma2)).collect()
-}
+} */
 
 pub fn laplace(b: f64, mu: f64, x: f64) -> f64 {
     (-(x - mu).abs() / b).exp() / (2.0 * b)
 }
 
-pub fn laplace_vec(b: f64, mu: f64, x: &[f64]) -> Vec<f64> {
+/* pub fn laplace_vec(b: f64, mu: f64, x: &[f64]) -> Vec<f64> {
     let frac_1_b: f64 = 1.0 / b;
     x.iter().map(|v| frac_1_b * (-(v - mu).abs() * frac_1_b).exp() / 2.0).collect()
-}
+} */
 
 pub fn logistic(mu: f64, s: f64, x: f64) -> f64 {
     basic::sech((x - mu) / (2.0 * s)).powi(2) / (4.0 * s)
 }
 
-pub fn logistic_vec(mu: f64, s: f64, x: &[f64]) -> Vec<f64> {
+/* pub fn logistic_vec(mu: f64, s: f64, x: &[f64]) -> Vec<f64> {
     let frac_1_s2: f64 = 1.0 / (2.0 * s);
     x.iter().map(|v| frac_1_s2 * basic::sech(frac_1_s2 * (v - mu)).powi(2) / 2.0 ).collect()
-}
+} */
 
 /// # Normal function
 /// 
@@ -78,7 +100,7 @@ pub fn logistic_vec(mu: f64, s: f64, x: &[f64]) -> Vec<f64> {
 /// assert!((res - 0.120985362259).abs() < 1e-12);
 /// ```
 pub fn normal(mu: f64, sigma: f64, x: f64) -> f64 {
-    (1.0 / (sigma * TAU.sqrt())) * (-(x - mu).powi(2) / (2.0 * sigma.powi(2))).exp()
+    (-0.5 * ((x - mu) / sigma).powi(2)).exp() / (sigma * TAU.sqrt())
 }
 
 /// # Gaussian function
